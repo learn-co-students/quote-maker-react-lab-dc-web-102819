7db89c290bc84deaaab2ba1a6316e9dc8ee753bc
github username <hanyc1205@gmail.com>
2020-08-15 10:50:00 -0400

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,57 @@
-import React from 'react';
+// import React from "react";
+// import {connect} from 'react-redux'
+// import {upVoting} from "../actions/quotes";
+// import { useDispatch } from "react-redux";
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
-      </div>
-    </div>
-  </div>;
+// const QuoteCard = (props) => {
+//   const { author, content, vote, id } = props.quote;
 
-export default QuoteCard;
+//   const dispatch = useDispatch();
+
+//   const handleUpVote = (id) => {
+//     dispatch(upVoting(id))
+//   };
+
+//   return (
+//     <div>
+//       <div className="card card-inverse card-success card-primary mb-3 text-center">
+//         <div className="card-block">
+//           <blockquote className="card-blockquote">
+//             {<p>{content}</p>}
+//             <footer>
+//               - author <cite title="Source Title">{author}</cite>
+//             </footer>
+//           </blockquote>
+//         </div>
+//         <div className="float-right">
+//           <div
+//             className="btn-group btn-group-sm"
+//             role="group"
+//             aria-label="Basic example"
+//           >
+//             <button
+//               type="button"
+//               className="btn btn-primary"
+//               onClick={() => handleUpVote(id)}
+//             >
+//               Upvote
+//             </button>
+//             <button type="button" className="btn btn-secondary">
+//               Downvote
+//             </button>
+//             <button type="button" className="btn btn-danger">
+//               <span aria-hidden="true">&times;</span>
+//             </button>
+//           </div>
+//           <div>Votes: {vote}</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const mapDispatchToProps = (dispatch) =>{
+//   upVoting : (id) => dispatch(upVoting(id))
+// } 
+
+// export default connect()(QuoteCard);
